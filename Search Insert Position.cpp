b670455563ff995b7ba9cd87f855f7913bbdac2d
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-11-01 00:08:13 +0530

    Create Search Insert Position.cpp

---
+++ Search Insert Position.cpp
@@ -0,0 +1,30 @@
+int searchInsert(vector<int>& arr, int m)
+{
+	    int low = 0, high = arr.size()-1;
+
+        int ans=arr.size();
+
+ 
+
+        while(low<=high) {
+
+            int mid = (low+high)/2;
+
+ 
+
+            if(arr[mid]>=m){
+
+                ans=mid;
+
+                high = mid-1;
+
+            }
+
+            else
+
+            low=mid+1;
+
+        }
+
+        return ans;
+}
