801d9738e173a94a8452f9082e6f30650552b40a
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-02-07 15:30:30 +0530

    Create  Count Digits.py

---
+++ Count Digits.py
@@ -0,0 +1,15 @@
+def countDigits(n: int) -> int:
+    # Initializing 'cur' and 'ans'.
+    cur = n
+    ans = 0
+
+    # Iterating while 'cur' > 0.
+    while cur > 0:
+        # 'd_0' gives us the digit at 'ones' place decimal notation of 'cur'.
+        d_0 = cur % 10
+        if d_0 != 0:
+            # Incrementing 'ans' by 1 if the current digit divides 'n' evenly.
+            ans += (n % d_0) == 0
+        cur //= 10
+
+    return ans
